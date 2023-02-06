75dca7c2b0cf7919e0373cd9a2f94269621490c4
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-02-06 20:33:17 +0000

    refactored up tp level 300

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//You can edit ALL of the code here
+// You can edit ALL of the code here
 // function setup() {
 //   const allEpisodes = getAllEpisodes();
 //   makePageForEpisodes(allEpisodes);
@@ -9,25 +9,9 @@
 //   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 // }
 
-// function makePageForEpisodes(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   rootElem.innerHTML = `${episodeList.name} S0${episodeList.season}E0${episodeList.number} <img src="${episodeList.image.medium}"> ${episodeList.summary}`;
-// }
-
-// const rootElem = document.getElementById("root");
-
-// function makePageForEpisodes(episodeList) {
-//   for (let i = 0; i < episodeList.length; i++) {
-//     const episode = episodeList[i];
-
-//     const divEl = `<div><h3>${episode.name}</h3></div>
-//     <div>S0${episode.season}E0${episode.number}</div>
-//     <div><img src="${episode.image.medium}"></div>
-//     <div>${episode.summary}</div>`;
-//     rootElem.innerHTML += divEl;
-//   }
-// }
 // window.onload = setup;
+
+// ------------------------------------------------------------------
 
 //You can edit ALL of the code here
 function setup() {
@@ -44,9 +28,9 @@ window.onload = setup;
 // initial variable (global scope)
 
 let list = document.createElement("ul");
-let count = 0;
 
 const select = document.createElement("select");
+
 const allOption = document.createElement("option");
 document.body.appendChild(select);
 
@@ -59,50 +43,29 @@ const countSpan = document.createElement("span");
 document.body.appendChild(countSpan);
 
 function displayEpisodesWithSearchBox(episodes) {
-
   //call back functions
   displayDropBox(episodes);
-  displayEpisodes(episodes);
-
-  countSpan.innerHTML = `Displaying ${count}/${episodes.length} episodes`;
+  displayEpisodes(episodes, episodes.length);
 
   searchInput.addEventListener("input", () => {
     const inputValue = searchInput.value.toLowerCase();
     list.innerHTML = "";
-    count = 0;
 
-    episodes.forEach((episode) => {
-      if (
+    let filterEpisodes = episodes.filter((episode) => {
+      const episodeDidMatch =
         episode.name.toLowerCase().includes(inputValue) ||
-        episode.summary.toLowerCase().includes(inputValue)
-      ) {
-        let li = document.createElement("li");
-        let title = document.createElement("h1");
-        let images = document.createElement("img");
-        let paragraph = document.createElement("p");
-        let episodeCode = `S${("0" + episode.season).slice(-2)}E${(
-          "0" + episode.number
-        ).slice(-2)}`;
+        episode.summary.toLowerCase().includes(inputValue);
 
-        title.innerHTML = `${episode.name} - ${episodeCode}`;
-        images.src = episode.image.medium;
-        paragraph.innerHTML = episode.summary;
-
-        document.body.appendChild(list);
-        list.appendChild(li);
-        li.appendChild(title);
-        li.appendChild(images);
-        li.appendChild(paragraph);
-        count++;
-      }
+      return episodeDidMatch;
     });
-    countSpan.innerHTML = `Displaying ${count}/${episodes.length} episodes`;
+
+    displayEpisodes(filterEpisodes, episodes.length);
   });
 }
 
 // initial display episodes (first function)
-function displayEpisodes(episodes) {
-  episodes.forEach((episode) => {
+function displayEpisodes(episodesToDisplay, totalNumberOfEpisodes) {
+  episodesToDisplay.forEach((episode) => {
     let li = document.createElement("li");
     let title = document.createElement("h1");
     let images = document.createElement("img");
@@ -120,8 +83,8 @@ function displayEpisodes(episodes) {
     li.appendChild(title);
     li.appendChild(images);
     li.appendChild(paragraph);
-    count++;
   });
+  countSpan.innerHTML = `Displaying ${episodesToDisplay.length}/${totalNumberOfEpisodes} episode(s)`;
 }
 
 //DropBox search bar
@@ -129,6 +92,7 @@ function displayDropBox(episodes) {
   allOption.value = "All";
   allOption.innerHTML = "All episodes";
   select.appendChild(allOption);
+
   episodes.forEach((episode) => {
     let episodeCode = `S${("0" + episode.season).slice(-2)}E${(
       "0" + episode.number
@@ -138,17 +102,16 @@ function displayDropBox(episodes) {
     eachOption.innerHTML = `${episodeCode} - ${episode.name}`;
     select.appendChild(eachOption);
   });
-    select.addEventListener("change", () => {
-      let newArr = [];
-      if (select.value === "All") {
-        newArr = episodes;
-      } else {
-        newArr = episodes.filter(episode => select.value.includes(episode.id))
-      }
-      count = 0;
-      list.innerHTML = "";
-      displayEpisodes(newArr);
-      countSpan.innerHTML = `Displaying ${count}/${episodes.length} episode(s)`
-    })
-  
+
+  select.addEventListener("change", () => {
+    let newArr = [];
+    if (select.value === "All") {
+      newArr = episodes;
+    } else {
+      newArr = episodes.filter((episode) => select.value.includes(episode.id));
+    }
+
+    list.innerHTML = "";
+    displayEpisodes(newArr, episodes.length);
+  });
 }
